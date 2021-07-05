dc798eca5b051d6d592203403e0b986e819add1c
Daniel Spelmezan <32591681+danhspe@users.noreply.github.com>
2021-07-05 18:13:30 +0200

    Add test cases

--- fizzBuzz/fizzBuzz_test.go
+++ fizzBuzz/fizzBuzz_test.go
@@ -8,6 +8,14 @@ import (
 	"github.io/fizz-buzz-rest-server/fizzBuzz"
 )
 
+type args struct {
+	int1  uint
+	int2  uint
+	limit uint
+	str1  string
+	str2  string
+}
+
 func TestFizzBuzz_WhenLimitIsLessThanOne_ThenReturnEmptySlice(t *testing.T) {
 	result := fizzBuzz.FizzBuzz(0, 0, 0, "", "")
 	assert.Empty(t, result, fmt.Sprintf("Expected empty slice - got %v", fmt.Sprintf("%v", result)))
@@ -32,10 +40,35 @@ func TestFizzBuzz_WhenInt1orInt2IsZero_ThenReturnEmptySlice(t *testing.T) {
 }
 
 func TestFizzBuzz_ReplaceMultiplesOfInt1WithStr1(t *testing.T) {
-	var expectedResult = fmt.Sprintf("%v", []string{"-1"})
+	tests := []struct {
+		args     args
+		expected string
+	}{
+		{args: args{
+			int1:  1,
+			int2:  1,
+			limit: 1,
+			str1:  "-1",
+			str2:  "",
+		}, expected: "[-1]"},
+		{args: args{
+			int1:  1,
+			int2:  1,
+			limit: 10,
+			str1:  "-1",
+			str2:  "",
+		}, expected: "[-1 -1 -1 -1 -1 -1 -1 -1 -1 -1]"},
+	}
 
-	result := fizzBuzz.FizzBuzz(1, 1, 1, "-1", "")
+	for i, tt := range tests {
+		name := fmt.Sprintf("Test %d", i)
+		t.Run(name, func(t *testing.T) {
 
-	resultString := fmt.Sprintf("%v", result)
-	assert.Equalf(t, expectedResult, resultString, "Expected %s - got %s", expectedResult, resultString)
+			result := fizzBuzz.FizzBuzz(tt.args.int1, tt.args.int2, tt.args.limit, tt.args.str1, tt.args.str2)
+
+			resultString := fmt.Sprintf("%v", result)
+			assert.Equalf(t, tt.expected, resultString, "Expected %s - got %s", tt.expected, resultString)
+
+		})
+	}
 }
