3541d4e721a94742ce37dcf3edd72e7b348951e9
Daniel Spelmezan <32591681+danhspe@users.noreply.github.com>
2021-07-05 17:29:39 +0200

    Test fizzBuzz when numbers are zero

--- fizzBuzz/fizzBuzz_test.go
+++ fizzBuzz/fizzBuzz_test.go
@@ -12,3 +12,16 @@ func TestFizzBuzz_WhenLimitIsLessThanOne_ThenReturnEmptySlice(t *testing.T) {
 	result := fizzBuzz.FizzBuzz(0, 0, 0, "", "")
 	assert.Empty(t, result, fmt.Sprintf("Expected empty slice - got %v", fmt.Sprintf("%v", result)))
 }
+
+func TestFizzBuzz_WhenInt1orInt2IsZero_ThenReturnEmptySlice(t *testing.T) {
+	var result []string
+
+	result = fizzBuzz.FizzBuzz(0, 0, 1, "", "")
+	assert.Empty(t, result, fmt.Sprintf("Expected empty slice - got %v", fmt.Sprintf("%v", result)))
+
+	result = fizzBuzz.FizzBuzz(1, 0, 1, "", "")
+	assert.Empty(t, result, fmt.Sprintf("Expected empty slice - got %v", fmt.Sprintf("%v", result)))
+
+	result = fizzBuzz.FizzBuzz(0, 1, 1, "", "")
+	assert.Empty(t, result, fmt.Sprintf("Expected empty slice - got %v", fmt.Sprintf("%v", result)))
+}
