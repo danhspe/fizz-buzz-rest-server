0800a599bdf3383c66bc2ddfe333249f3517fd0b
Daniel Spelmezan <32591681+danhspe@users.noreply.github.com>
2021-07-05 18:53:19 +0200

    Update fizzBuzz

--- fizzBuzz/fizzBuzz.go
+++ fizzBuzz/fizzBuzz.go
@@ -1,5 +1,7 @@
 package fizzBuzz
 
+import "strconv"
+
 // FizzBuzz returns a list of strings with numbers from 1 to limit, where:
 // all multiples of int1 are replaced by str1, all multiples of int2 are replaced by str2,
 // all multiples of int1 and int2 are replaced by str1str2.
@@ -11,9 +13,15 @@ func FizzBuzz(int1 uint, int2 uint, limit uint, str1 string, str2 string) []stri
 	}
 
 	for i := uint(1); i <= limit; i++ {
-		if i%int1 == 0 {
-			result = append(result, str1)
+		replacement := strconv.Itoa(int(i))
+		if numberIsDivisibleOnlyByInt1(i, int1, int2) {
+			replacement = str1
+		} else if numberIsDivisibleOnlyByInt2(i, int1, int2) {
+			replacement = str2
+		} else if numberIsDivisibleByInt1AndInt2(i, int1, int2) {
+			replacement = str1 + str2
 		}
+		result = append(result, replacement)
 	}
 
 	return result
