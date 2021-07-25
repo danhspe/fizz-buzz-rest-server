9ddea563c6ac8eea329a7fb13f2f04539c98f469
Daniel Spelmezan <32591681+danhspe@users.noreply.github.com>
2021-07-25 18:26:36 +0200

    Update main imports

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/danhspe/fizz-buzz-rest-server/golib/fizzbuzz"
 	"github.com/danhspe/fizz-buzz-rest-server/internal/cache"
 	"github.com/danhspe/fizz-buzz-rest-server/internal/cache/redis"
-	grpc2 "github.com/danhspe/fizz-buzz-rest-server/layers/grpc"
+	grpc2 "github.com/danhspe/fizz-buzz-rest-server/layers/api/grpc"
 	fizzbuzz3 "github.com/danhspe/fizz-buzz-rest-server/layers/repositories/fizzbuzz"
 	"github.com/danhspe/fizz-buzz-rest-server/layers/repositories/statistics"
 	fizzbuzz2 "github.com/danhspe/fizz-buzz-rest-server/layers/usecases/fizzbuzz"
