6c0327ab3e7b00955a8ee8bf18831bfa8218542d
Daniel Spelmezan <32591681+danhspe@users.noreply.github.com>
2021-07-27 17:55:17 +0200

    Document function

--- internal/fizzBuzz/fizzBuzz.go
+++ internal/fizzBuzz/fizzBuzz.go
@@ -5,6 +5,7 @@ import "strconv"
 // FizzBuzz returns a list of strings with numbers from 1 to limit, where:
 // all multiples of int1 are replaced by str1, all multiples of int2 are replaced by str2,
 // all multiples of int1 and int2 are replaced by str1str2.
+// Returns an empty array when either int1, int2, or the limit is less than 1.
 func FizzBuzz(int1 int, int2 int, limit int, str1 string, str2 string) []string {
 	var result []string
 
