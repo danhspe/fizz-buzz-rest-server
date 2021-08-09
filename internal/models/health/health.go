c03c927e3b9cb1f787b1ccaa318a346ac03998a2
Daniel Spelmezan <32591681+danhspe@users.noreply.github.com>
2021-08-09 18:00:11 +0200

    Define health messages

--- internal/models/health/health.go
+++ internal/models/health/health.go
@@ -1,5 +1,11 @@
 package health
 
+var (
+	MessageHealthy  = "healthy"
+	MessageReady    = "ok"
+	MessageNotReady = "not ok"
+)
+
 type Health interface {
 	Healthy() (string, error)
 	Ready() (string, error)
